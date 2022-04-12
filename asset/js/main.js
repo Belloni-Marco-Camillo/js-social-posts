766e9854c0eed7863e3c61ab945b94bcf0d3801c
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-04-12 15:58:35 +0200

    initial_commit

---
+++ asset/js/main.js
@@ -0,0 +1,39 @@
+/* Milestone 1 */
+//Creiamo il nostro array di oggetti che rappresentano ciascun post. 
+//Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
+/* id del post, numero progressivo da 1 a n
+nome autore,
+foto autore,
+data in formato americano (mm-gg-yyyy),
+testo del post,
+immagine (non tutti i post devono avere una immagine),
+numero di likes.
+Non è necessario creare date casuali
+Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
+ */
+
+/* const posts = [{
+    id: 0001,
+    Author: `marco`,
+    PhotoAuthor: `https://picsum.photos/id/237/200/300`,
+    Data: 05 / 10 / 1992,
+    Text: `testo di prova`,
+    ImmaginePost: `https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI`,
+    like: 0
+}, {
+    id: 0002,
+    Author: `marco`,
+    PhotoAuthor: `https://picsum.photos/id/237/200/300`,
+    Data: 05 / 10 / 1992,
+    Text: `testo di prova`,
+    ImmaginePost: `https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI`,
+    like: 0
+}]; */
+
+function createPost() {
+    const dataPost = new data();
+}
+createPost();
+console.log(dataPost);
+
+/* console.log(posts); */
