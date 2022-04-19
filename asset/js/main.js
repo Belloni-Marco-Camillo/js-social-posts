437804d5722cb527422649836aa09f54ef100a88
Belloni-Marco-Camillo <bellonimarcocamillo@gmail.com>
2022-04-19 15:29:44 +0200

    commits_mancanti

--- asset/js/main.js
+++ asset/js/main.js
@@ -1,39 +1,75 @@
-/* Milestone 1 */
-//Creiamo il nostro array di oggetti che rappresentano ciascun post. 
-//Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
-/* id del post, numero progressivo da 1 a n
-nome autore,
-foto autore,
-data in formato americano (mm-gg-yyyy),
-testo del post,
-immagine (non tutti i post devono avere una immagine),
-numero di likes.
-Non è necessario creare date casuali
-Per le immagini va bene utilizzare qualsiasi servizio di placeholder ad es. Unsplash (https://unsplash.it/300/300?image=<id>)
- */
+//creo un array di oggetti
+const posts = [{
+        post_id: 1,
+        autoreName: `marco`,
+        autoreSeconName: `belloni`,
+        data: `21/09/2021`,
+        ImageProfile: `https://picsum.photos/id/237/200/200`,
+        testo_post: `prova testo`,
+        Image: `https://picsum.photos/id/27/800/500`,
+        like: 0,
+    },
+    {
+        post_id: 2,
+        autoreName: `giuseppe`,
+        autoreSeconName: `belloni`,
+        data: `05/09/2021`,
+        ImageProfile: `https://picsum.photos/id/27/200/200`,
+        testo_post: `prova testo di giuseppe`,
+        Image: `https://picsum.photos/id/237/800/500`,
+        like: 0,
+    },
+    {
+        post_id: 3,
+        autoreName: `luisa`,
+        autoreSeconName: `belloni`,
+        data: `05/09/2021`,
+        ImageProfile: `https://picsum.photos/id/27/200/200`,
+        testo_post: `prova testo di giuseppe`,
+        Image: ``,
+        like: 0,
+    }
+];
 
-/* const posts = [{
-    id: 0001,
-    Author: `marco`,
-    PhotoAuthor: `https://picsum.photos/id/237/200/300`,
-    Data: 05 / 10 / 1992,
-    Text: `testo di prova`,
-    ImmaginePost: `https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI`,
-    like: 0
-}, {
-    id: 0002,
-    Author: `marco`,
-    PhotoAuthor: `https://picsum.photos/id/237/200/300`,
-    Data: 05 / 10 / 1992,
-    Text: `testo di prova`,
-    ImmaginePost: `https://i.picsum.photos/id/866/200/300.jpg?hmac=rcadCENKh4rD6MAp6V_ma-AyWv641M4iiOpe1RyFHeI`,
-    like: 0
-}]; */
 
-function createPost() {
-    const dataPost = new data();
+
+
+i = 0
+while (i <= posts.length) {
+    const post = document.createElement(`div`);
+    post.classList.add(`post`)
+    post.classList.add(`col-8`)
+    post.classList.add(`offset-2`)
+    post.classList.add(`my-5`)
+    post.classList.add(`shadowPost`)
+    post.innerHTML =
+        `
+        <div class="post_header px-4">
+            <div class="datiAutore d-flex">
+                <div class="col-1 py-4 me-4 ">
+                    <img class="imgProfile img-fluid" src="${posts[i].ImageProfile}" width=200px>
+                </div>
+                <div class="specifiche d-flex align-items-center">
+                <div>${posts[i].autoreName} ${posts[i].autoreSeconName}</div>  <div>${posts[i].data}</div>
+                </div>
+            </div>
+            <div class="testoPost">
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Corrupti perspiciatis suscipit ea itaque! Aspernatur id dolorem fugit impedit praesentium neque repellendus rerum maxime placeat aut.</p>
+            </div>
+        </div>
+
+        <div class="post_body">
+        <img class="img-fluid px-4" src="${posts[i].Image}">
+        </div>
+        <div class="post_footer d-flex text-center">
+            <div class="like_it col-6">
+                <button id="" class="btnLike px-5 py-2 m-3"><i class="fa-solid fa-thumbs-up"></i> metti like</button>
+            </div>
+            <div id="" class=" numlike col-6 text-center"></div>
+        </div>
+    `;
+
+
+    document.querySelector(`.timeLine`).appendChild(post)
+    i++
 }
-createPost();
-console.log(dataPost);
-
-/* console.log(posts); */
